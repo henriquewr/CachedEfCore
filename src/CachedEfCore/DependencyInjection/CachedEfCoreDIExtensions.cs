39c45869e2562b2344abd368f4745b0c7b5ed2e4
Henrique Ruschel <henrique.2004.hwr@gmail.com>
2025-08-11 02:54:13 -0300

    Add Di ConfigHelper

---
+++ src/CachedEfCore/DependencyInjection/CachedEfCoreDIExtensions.cs
@@ -0,0 +1,27 @@
+﻿using CachedEfCore.Cache;
+using CachedEfCore.Cache.Helper;
+using CachedEfCore.Interceptors;
+using CachedEfCore.KeyGeneration;
+using CachedEfCore.KeyGeneration.ExpressionKeyGen;
+using CachedEfCore.SqlAnalysis;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace CachedEfCore.DependencyInjection
+{
+    public static class CachedEfCoreDIExtensions
+    {
+        public static IServiceCollection AddCachedEfCore<TSqlQueryEntityExtractor>(this IServiceCollection serviceCollection) where TSqlQueryEntityExtractor : class, ISqlQueryEntityExtractor
+        {
+            serviceCollection.AddMemoryCache();
+            serviceCollection.TryAddSingleton<IPrintabilityChecker, PrintabilityChecker>();
+            serviceCollection.TryAddSingleton<KeyGeneratorVisitor>();
+            serviceCollection.TryAddSingleton<IDbQueryCacheHelper, DbQueryCacheHelper>();
+            serviceCollection.TryAddSingleton<IDbQueryCacheStore, DbQueryCacheStore>();
+            serviceCollection.TryAddSingleton<ISqlQueryEntityExtractor, TSqlQueryEntityExtractor>();
+            serviceCollection.TryAddSingleton<DbStateInterceptor>();
+
+            return serviceCollection;
+        }
+    }
+}
