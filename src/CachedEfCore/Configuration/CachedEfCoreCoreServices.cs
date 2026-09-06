9d084daab61351c5ae1cd90698835bf176325592
Henrique Ruschel <henrique.2004.hwr@gmail.com>
2026-09-06 20:41:51 -0300

    Extensible service registration
    fixes #25

---
+++ src/CachedEfCore/Configuration/CachedEfCoreCoreServices.cs
@@ -0,0 +1,177 @@
+﻿using CachedEfCore.Cache;
+using CachedEfCore.Cache.Helper;
+using CachedEfCore.Cache.Metrics;
+using CachedEfCore.DependencyManager;
+using CachedEfCore.EntityMapping;
+using CachedEfCore.Interceptors;
+using CachedEfCore.KeyGeneration;
+using CachedEfCore.KeyGeneration.ExpressionEvaluation;
+using CachedEfCore.KeyGeneration.ExpressionEvaluation.EvalTypeChecker;
+using CachedEfCore.KeyGeneration.ExpressionKeyGen;
+using CachedEfCore.KeyGeneration.TypeCompatibility;
+using CachedEfCore.SqlAnalysis;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+namespace CachedEfCore.Configuration
+{
+    public static class CachedEfCoreCoreServices
+    {
+        public static IEnumerable<CachedEfCoreService> GetCoreServices()
+        {
+            yield return new CachedEfCoreService
+            {
+                ServiceDescriptor = ServiceDescriptor.Scoped<EntityDependency>(sp =>
+                {
+                    var dbContext = sp.GetRequiredService<ICurrentDbContext>().Context;
+
+                    var entityDependency = EntityDependency.GetOrAdd(dbContext.Model);
+
+                    return entityDependency;
+                }),
+                GetServiceProviderHashCode = null,
+                ShouldUseSameServiceProvider = null,
+                Options = null,
+            };
+
+            yield return new CachedEfCoreService
+            {
+                ServiceDescriptor = ServiceDescriptor.Scoped<TableEntityMapping>(sp =>
+                {
+                    var dbContext = sp.GetRequiredService<ICurrentDbContext>().Context;
+
+                    var tableEntityMapping = TableEntityMapping.GetOrAdd(dbContext.Model);
+
+                    return tableEntityMapping;
+                }),
+                GetServiceProviderHashCode = null,
+                ShouldUseSameServiceProvider = null,
+                Options = null,
+            };
+
+            yield return new CachedEfCoreService
+            {
+                ServiceDescriptor = ServiceDescriptor.Singleton<IPrintabilityChecker, PrintabilityChecker>(),
+                GetServiceProviderHashCode = null,
+                ShouldUseSameServiceProvider = null,
+                Options = null,
+            };
+
+            yield return new CachedEfCoreService
+            {
+                ServiceDescriptor = ServiceDescriptor.Singleton<IExpressionEvalTypeChecker, ExpressionEvalTypeCheckerVisitor>(),
+                GetServiceProviderHashCode = null,
+                ShouldUseSameServiceProvider = null,
+                Options = null,
+            };
+
+            yield return new CachedEfCoreService
+            {
+                ServiceDescriptor = ServiceDescriptor.Singleton<ICachedEfCoreEvalutableExpressionChecker, CachedEfCoreEvalutableExpressionChecker>(),
+                GetServiceProviderHashCode = null,
+                ShouldUseSameServiceProvider = null,
+                Options = null,
+            };
+
+            {
+                var defaultJsonSerializerOptions = CachedEfCoreKeyGenerationOptionsDefaults.DefaultJsonSerializerOptions;
+                yield return new CachedEfCoreService
+                {
+                    ServiceDescriptor = ServiceDescriptor.Scoped<KeyGeneratorVisitor>(sp =>
+                    {
+                        var printabilityChecker = sp.GetRequiredService<IPrintabilityChecker>();
+                        var model = sp.GetRequiredService<IModel>();
+                        var cachedEfCoreEvalutableExpressionChecker = sp.GetRequiredService<ICachedEfCoreEvalutableExpressionChecker>();
+
+                        return new KeyGeneratorVisitor(
+                            printabilityChecker,
+                            model,
+                            cachedEfCoreEvalutableExpressionChecker,
+                            defaultJsonSerializerOptions
+                        );
+                    }),
+                    GetServiceProviderHashCode = thisService => ((JsonSerializerOptions)thisService.Options!).GetHashCode(),
+                    ShouldUseSameServiceProvider = args => ((JsonSerializerOptions)args.ThisService.Options!).Equals(((JsonSerializerOptions)args.OtherServices.Single().Options!)),
+                    Options = defaultJsonSerializerOptions,
+                };
+            }
+
+            {
+                var defaultNonEvaluableTypes = CachedEfCoreKeyGenerationOptionsDefaults.DefaultNonEvaluableTypes;
+                yield return new CachedEfCoreService
+                {
+                    ServiceDescriptor = ServiceDescriptor.Singleton<ITypeCompatibilityChecker>(sp =>
+                    {
+                        return new TypeCompatibilityChecker(defaultNonEvaluableTypes);
+                    }),
+                    GetServiceProviderHashCode = thisService => ((List<Type>)thisService.Options!).GetHashCode(),
+                    ShouldUseSameServiceProvider = args => ((List<Type>)args.ThisService.Options!).SequenceEqual(((List<Type>)args.OtherServices.Single().Options!)),
+                    Options = defaultNonEvaluableTypes,
+                };
+            }
+
+            yield return new CachedEfCoreService
+            {
+                ServiceDescriptor = ServiceDescriptor.Singleton<IDbQueryCacheHelper, DbQueryCacheHelper>(),
+                GetServiceProviderHashCode = null,
+                ShouldUseSameServiceProvider = null,
+                Options = null,
+            };
+
+            yield return new CachedEfCoreService
+            {
+                ServiceDescriptor = ServiceDescriptor.Singleton<IDbQueryCacheMetrics>(sp =>
+                {
+                    return new DbQueryCacheWithGlobalMetrics(new DbQueryCacheMetrics());
+                }),
+                GetServiceProviderHashCode = null,
+                ShouldUseSameServiceProvider = null,
+                Options = null,
+            };
+
+            yield return new CachedEfCoreService
+            {
+                ServiceDescriptor = ServiceDescriptor.Singleton<IDbQueryCacheInternalStore, DbQueryCacheInternalStore>(),
+                GetServiceProviderHashCode = null,
+                ShouldUseSameServiceProvider = null,
+                Options = null,
+            };
+
+            yield return new CachedEfCoreService
+            {
+                ServiceDescriptor = ServiceDescriptor.Scoped<IDbQueryCacheStore>(sp =>
+                {
+                    var dbContext = sp.GetRequiredService<ICurrentDbContext>().Context;
+                    return new DbQueryCacheStore(dbContext);
+                }),
+                GetServiceProviderHashCode = null,
+                ShouldUseSameServiceProvider = null,
+                Options = null,
+            };
+
+            {
+                var sqlQueryEntityExtractorType = typeof(GenericSqlQueryEntityExtractor);
+                yield return new CachedEfCoreService
+                {
+                    ServiceDescriptor = ServiceDescriptor.Singleton(typeof(ISqlQueryEntityExtractor), sqlQueryEntityExtractorType),
+                    GetServiceProviderHashCode = thisService => ((Type)thisService.Options!).GetHashCode(),
+                    ShouldUseSameServiceProvider = args => ((Type)args.ThisService.Options!) == ((Type)args.OtherServices.Single().Options!),
+                    Options = sqlQueryEntityExtractorType,
+                };
+            }
+
+            yield return new CachedEfCoreService
+            {
+                ServiceDescriptor = ServiceDescriptor.Singleton<DbStateInterceptor, DbStateInterceptor>(),
+                GetServiceProviderHashCode = null,
+                ShouldUseSameServiceProvider = null,
+                Options = null,
+            };
+        }
+    }
+}
--- src/CachedEfCore/Configuration/CachedEfCoreKeyGenerationOptionsBuilder.cs
+++ src/CachedEfCore/Configuration/CachedEfCoreKeyGenerationOptionsBuilder.cs
@@ -1,35 +1,73 @@
-﻿using System;
+﻿using CachedEfCore.DbContextOptionExtensions;
+using CachedEfCore.KeyGeneration;
+using CachedEfCore.KeyGeneration.ExpressionEvaluation;
+using CachedEfCore.KeyGeneration.ExpressionKeyGen;
+using CachedEfCore.KeyGeneration.TypeCompatibility;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
 
 namespace CachedEfCore.Configuration
 {
     public class CachedEfCoreKeyGenerationOptionsBuilder
     {
-        protected CachedEfCoreKeyGenerationOptions KeyGenerationOptions { get; }
+        private readonly CachedEfCoreDbContextOptionExtension _cachedEfCoreExtension;
 
-        public CachedEfCoreKeyGenerationOptionsBuilder(CachedEfCoreKeyGenerationOptions keyGenerationOptions)
+        public CachedEfCoreKeyGenerationOptionsBuilder(CachedEfCoreDbContextOptionExtension cachedEfCoreExtension)
         {
-            KeyGenerationOptions = keyGenerationOptions;
+            _cachedEfCoreExtension = cachedEfCoreExtension;
         }
 
         public virtual CachedEfCoreKeyGenerationOptionsBuilder ConfigureJsonSerializer(Func<JsonSerializerOptions, JsonSerializerOptions> configure)
         {
-            KeyGenerationOptions.JsonSerializerOptions = configure(CachedEfCoreKeyGenerationOptions.DefaultJsonSerializerOptions);
+            var jsonSerializerOptions = configure(CachedEfCoreKeyGenerationOptionsDefaults.DefaultJsonSerializerOptions);
+
+            var service = new CachedEfCoreService
+            {
+                ServiceDescriptor = ServiceDescriptor.Scoped<KeyGeneratorVisitor>(sp =>
+                {
+                    var printabilityChecker = sp.GetRequiredService<IPrintabilityChecker>();
+                    var model = sp.GetRequiredService<IModel>();
+                    var cachedEfCoreEvalutableExpressionChecker = sp.GetRequiredService<ICachedEfCoreEvalutableExpressionChecker>();
+
+                    return new KeyGeneratorVisitor(
+                        printabilityChecker,
+                        model,
+                        cachedEfCoreEvalutableExpressionChecker,
+                        jsonSerializerOptions
+                    );
+                }),
+                GetServiceProviderHashCode = thisService => ((JsonSerializerOptions)thisService.Options!).GetHashCode(),
+                ShouldUseSameServiceProvider = args => ((JsonSerializerOptions)args.ThisService.Options!).Equals(((JsonSerializerOptions)args.OtherServices.Single().Options!)),
+                Options = jsonSerializerOptions,
+            };
+
+            _cachedEfCoreExtension.ReplaceService(service);
 
             return this;
         }
 
         public virtual CachedEfCoreKeyGenerationOptionsBuilder ConfigureNonEvaluableTypes(Func<List<Type>, List<Type>> configure)
         {
-            KeyGenerationOptions.NonEvaluableTypes = configure(CachedEfCoreKeyGenerationOptions.DefaultNonEvaluableTypes);
+            var nonEvaluableTypes = configure(CachedEfCoreKeyGenerationOptionsDefaults.DefaultNonEvaluableTypes);
+
+            var service = new CachedEfCoreService
+            {
+                ServiceDescriptor = ServiceDescriptor.Singleton<ITypeCompatibilityChecker>(sp =>
+                {
+                    return new TypeCompatibilityChecker(nonEvaluableTypes);
+                }),
+                GetServiceProviderHashCode = thisService => ((List<Type>)thisService.Options!).GetHashCode(),
+                ShouldUseSameServiceProvider = args => ((List<Type>)args.ThisService.Options!).SequenceEqual(((List<Type>)args.OtherServices.Single().Options!)),
+                Options = nonEvaluableTypes,
+            };
+
+            _cachedEfCoreExtension.ReplaceService(service);
 
             return this;
-        }
-
-        public CachedEfCoreKeyGenerationOptions Build()
-        {
-            return KeyGenerationOptions;
         }
     }
 }
